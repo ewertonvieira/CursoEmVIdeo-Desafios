863a3172b84ce37662db5137d1f53e183ecd0e8f
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-21 14:37:53 -0300

    Res: Desafio17

---
+++ Desafio17.py
@@ -0,0 +1,4 @@
+from math import sqrt
+catetooposto = float(input("Informe o valor do cateto oposto em centimetros(0.10m) ou metros(ex: 1.0m) >>> "))
+catetoadjacente = float(input("Informe o valor do cateto adjacente em centimetros(0.10cm) ou metros(ex: 1.0m) >>> "))
+print(f"Informe o valor do carteto oposto em centimetros(0.10m) ou metros(ex: 1.0m) >>> é {sqrt((catetooposto * catetooposto) + (catetoadjacente * catetoadjacente))}")
