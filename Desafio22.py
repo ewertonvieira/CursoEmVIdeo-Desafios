3ab79939fb5e9e63da0af72e4471ead6eb482815
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-25 10:33:06 -0300

    Res: Desafio22

--- Desafio22.py
+++ Desafio22.py
@@ -1,12 +1,12 @@
 name = input("Informe seu nome completo: ")
-# O nome de todas as letras MAICUSCULAS
+# O nome de todas as letras MAICUSCULAS;
 print(f'Nome caixa alta: {name.upper()}')
 # O nome com todas minusculas
 print(f'Nome caixa baixa: {name.lower()}')
-# Quantas letras ao todo, sem contar espacos
+# Quantas letras ao todo, sem contar espacos;
 namenospace = name.replace(" ", "")
 print (len(namenospace))
-# quantas letras tem o primeiro nome.
+# quantas letras tem o primeiro nome;
 name = name.lstrip()
 fistname = name[:name.find(" ")]
 print(f'Numero de letras do primeiro nome: {len(fistname)} | Primeiro nome: {fistname} ')
