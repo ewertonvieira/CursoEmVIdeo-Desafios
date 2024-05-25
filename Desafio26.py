59cea41fda12154bd69c1f884eaa44c50c69fe6f
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-25 15:53:49 -0300

    Res: Desafio26

---
+++ Desafio26.py
@@ -0,0 +1,8 @@
+frase = input('Informe uma frase: ')
+frase = frase.upper()
+# Quantas vezes aparece a letra A:
+print(f'A letra "a" aparece: {frase.count('A')} vezes')
+# Em qual posicao a letra a aparece pela primeira vez
+print(f'A letra "a" aparece primeiro na prosicao: {frase.find('A')}')
+# Em qual posicao a letra a aparece pela ultima vez
+print(f'A letra "a" aparece pela ultima vez na posicao: {frase.rfind('A')}')
