d019e5c1d76fdc17352e6a067b7cdb50ca753e0a
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-26 13:19:55 -0300

    Res: Desafio34

--- Desafio34.py
+++ Desafio34.py
@@ -1,2 +1,2 @@
 salario = float(input('Informe seu salario atual R$: '))
-print(f'Aumento salario: {((12/100)*salario)+salario} R$' if salario > 1250 else f'Aumento salarial: {((15/100)*salario)+salario} R$')
+print(f'Aumento salario: {((10/100)*salario)+salario} R$' if salario > 1250 else f'Aumento salarial: {((15/100)*salario)+salario} R$')
