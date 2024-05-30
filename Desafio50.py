eced5697d017d18a2a327f397c7e0c29b708e089
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-29 21:24:23 -0300

    Res: Desafio51 | Mundo II

--- Desafio50.py
+++ Desafio50.py
@@ -2,5 +2,5 @@ soma = 0
 num = int(input('INFORME UM NUMERO: '))
 for num in range(0, num + 1):
     if num % 2 == 0:
-        soma += num
+        soma = soma + num
 print(f"Soma dos numeros pares no intervalo de 0 a '{num}'... {soma}")
