d5c198616361b9e73905f42dc78bd17341cf8216
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-31 16:05:52 -0300

    Res: Desafio54 | Mundo II

--- Desafio54.py
+++ Desafio54.py
@@ -1,22 +1,25 @@
-from datetime import datetime
-from os import system
-# Instanciando ano_atual e a lista dataniver
-cont = 0; cont_f = 0
-ano_atual = datetime.now().year
-dataaniver= [None] * 7; novadata=[None] * 7
-system('cls')
-print('Data de niversario(dd, mm, aa)')
-for data in range(len(dataaniver)):
-    dia = int(input('Informe dia: '))
-    mes = int(input('Informe mes:'))
-    ano = int(input('Informe ano:'))
-    print('_=_'*20)
-    dataaniver[data] = datetime(day=dia, month=mes, year=ano)
-system('cls')
-for i in range(len(dataaniver)):
-    anos = ano_atual - dataaniver[i].year
-    if anos>= 21:
-        cont += 1
+from datetime import datetime, timedelta
+data_aniversario = [None] * 7;
+data_now = datetime.now()
+cont_21 = 0; cont_min = 0
+#
+print('--'*30)
+print('Informe sua data de aniversario no formato (dd - mm - aa): \n')
+# Inserindo as capturas de datas em uma lista de 7 espacos
+for i in range(len(data_aniversario)):
+    dia = int(input('Dia: '))
+    mes = int(input('Mes: '))
+    ano = int(input('Ano: '))
+    print('--'*20)
+    data_aniversario[i] =  datetime(day=dia, month=mes, year=ano)
+# Percorrendo a lista de datas
+for j in range(len(data_aniversario)):
+    # Pegando a diferenca de dias entre a data atual e a data de aniversario informanda anteriormente
+    dias_bith = (data_now - data_aniversario[j]).days
+    # Comparando a diferenca de dias anterior a o numero de dias que 21 anos tem
+    if dias_bith >= timedelta(21*365).days:
+        cont_21 += 1
     else:
-        cont_f += 1
-print(f'No fim {cont_f} nao atingiram a maior idade enquanto {cont} ja atingiram.')
+        cont_min += 1
+print("Numero de pessoas maiores de 21 anos e menores presente na lista:")
+print(f"Maiores: {cont_21}\nMenores: {cont_min}")
