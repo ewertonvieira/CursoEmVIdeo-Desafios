0961e94f460aea13e905709590cc86cb827be437
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-01 18:58:16 -0300

    Res: Desafio31

--- Desafio31.py
+++ Desafio31.py
@@ -1,3 +1,2 @@
 dist = float(input("Informe a distancia percorrida em (km): "))
-print(f'Valor da passagem: {dist*0.50} R$'if dist<=200 else f'Valor da passagem: {dist*0.45} R$' )
-    
+print(f'Valor da passagem: {dist*0.50} R$'if dist<=200 else f'Valor da passagem: {dist*0.45} R$ ' )
