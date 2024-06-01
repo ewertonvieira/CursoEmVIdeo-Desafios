e0be86530157d5abf5acc0df297483ab4e89ec1f
ewertonvieira <ewertonvieira2401@gmail.com>
2024-05-31 21:46:02 -0300

    Res: Desafio56 | Mundo II

--- Desafio56.py
+++ Desafio56.py
@@ -1,26 +1,34 @@
 from os import system as sy
-lista_pessoas = [None] * 2; tam = len(lista_pessoas); soma = 0
-idademedia = None
+lista_pessoas = [None] * 4; count = 0; soma = 0
+size = len(lista_pessoas)
 #
 sy('cls')
-for i in range(tam):
-    nome = input('Informe nome: ')
-    idade = input('Informe idade: ')
-    op = input('Informe sexo:\nM ou m - Masculino\nF ou f - Feminino\n>> ')
-    if op == 'm' or 'M':
-        sexo = "MASCULINO"
-    if op == 'f' or 'F':
-        sexo = 'FEMININO'
-    else:
-        print('Erro de entrada!')
-        input('Pessione qualquer tecla para sair...\n')
-    sy('cls')
-    # Lista: lista_pessoas esta recebendo um dicionario com 3 chaves e 3 valores: nome, idade e sexo
-    lista_pessoas[i] = [{'nome' : nome, 'idade' : int(idade), 'sexo' : sexo}]
-# Media de idade
-for j in range(tam):
-    enum = lista_pessoas[j][0]['idade']
-    soma += enum
-print(f'Media de idade: {soma/tam:.0f} anos')
-# Qual o nome do Homem mais velho
-# Quantas mulheres tem menor de 21 anos
+for i in range(size):
+    nome = str(input('Informe nome: '))
+    idade = int(input('Informe idade: '))
+    op = int(input('Menu:\n1 - Masculino | 2 - Femino\n>> '))
+    if op == 1:
+        sexo = 'masculino'
+    if op == 2:
+        sexo = 'feminino'
+    print('-'*30)
+    lista_pessoas[i] = [{"nome" : nome, "idade" : idade, "sexo" : sexo}]
+
+sy('cls')
+for j in range(0, size):
+    idade = lista_pessoas[j][0]["idade"]
+    soma = idade + soma
+print(f'Media de idade: {soma/size:.2f} anos\n')
+print('-'*30)
+
+lista_pessoas.sort(key=lambda x: x[0]["idade"])
+
+for jk in range(0, size):
+    if lista_pessoas[jk][0]["idade"] < 21:
+        count += 1
+    if lista_pessoas[jk][0]["sexo"] == 'masculino':
+        print(f"Nome do homem mais velho: {lista_pessoas[size-1][0]["nome"]}\n")
+        print('-'*30)
+        break
+print(f"Mulheres menores de 21: {count}\n")
+print('-'*30)
