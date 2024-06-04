36e206339129fa234d6a71468fb58d45f48904ed
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-03 22:45:57 -0300

    Res: Desafio60 | Mundo II

--- Desafio60.py
+++ Desafio60.py
@@ -4,10 +4,10 @@ fat = 1; res = 0
 num = int(input("Informe numero para fatorial: "))
 if num < 0:
     print("Numero negativo nao tem fatorial!")
-elif num % 2 != 0:
+else:
     i = 1
     while i <= num:
         fat *= i
         i += 1
-print(fat)
+    print(fat)
         
