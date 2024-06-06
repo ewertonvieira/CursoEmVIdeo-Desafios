660cdf95f4c76c6a3bf8cf7c35e13e4b0d161163
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-05 21:50:59 -0300

    Res: Desafio74 | Mundo II

--- Desafio74.py
+++ Desafio74.py
@@ -2,7 +2,7 @@ from random import randint
 
 lista_num = ()
 for index in range(0,4):
-    num = randint(1, 5)
+    num = randint(1, 10)
     lista_num = lista_num + (num,)
 print("Tupla gerada: ",lista_num)
 new_lista_num = tuple(sorted(lista_num))
