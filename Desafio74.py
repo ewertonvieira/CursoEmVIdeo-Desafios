f070821cc1f9baa37824c93711cf54814589a88f
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-05 21:48:52 -0300

    Res: Desafio74 | Mundo II

---
+++ Desafio74.py
@@ -0,0 +1,9 @@
+from random import randint
+
+lista_num = ()
+for index in range(0,4):
+    num = randint(1, 5)
+    lista_num = lista_num + (num,)
+print("Tupla gerada: ",lista_num)
+new_lista_num = tuple(sorted(lista_num))
+print(f"Maior numero: {new_lista_num[3]} | Menor numero: {new_lista_num[0]}")
