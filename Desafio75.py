579de76e2b1882765c289a9812c5bcd4eb58da48
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-05 22:07:25 -0300

    Res: Desafio75 | Mundo II

--- Desafio75.py
+++ Desafio75.py
@@ -4,7 +4,13 @@ for i in range(0,4):
     lista_num = lista_num + (num, )
     if num % 2 == 0:
         lista_pares = lista_pares + (num,)
-        
-print("Vezes em que o 9 apareceu: ",lista_num.count(9))
-print("Primeira posicao em que 3 foi digitado: ",lista_num.index(3))
-print("Quais sao numeros pares: ", lista_pares)
+try: 
+    if lista_num.count(9) == 0:
+        print("Nao temos numero 9 presente na tuplas")
+    else:    
+        print("Vezes em que o 9 apareceu: ",lista_num.count(9))
+    print("Primeira posicao em que 3 foi digitado: ",lista_num.index(3))
+    print("Quais sao numeros pares: ", lista_pares)
+except ValueError:
+    print("Numero 3 nao esta presente na tupla")
+    print("Nao temos numeros pares na tupla")
