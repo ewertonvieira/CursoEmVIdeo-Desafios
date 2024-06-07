2471aff26fd4ec302098822c0155eb926d376ec3
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-07 17:53:49 -0300

    Res: Desafio77 | Mundo II

--- Desafio77.py
+++ Desafio77.py
@@ -3,10 +3,10 @@ tupla_nome = ("Abacaxi", "Banana", "Cachorro", "Domingo", "Elefante", "Felicidad
               "Piano", "Queijo", "Rato", "Sol", "Tigre", "Uva", "Vela", "Xadrez", "Yoga", "Zebra",
               "Arco-iris", "Borboleta", "Ceu", "Dente")
 for palavras in tupla_nome:
+    print(f"\n{palavras}: ",end="")
     vogais = ""
-    for i in range(len(palavras)):
-        if  palavras[i].lower() == 'a' or palavras[i].lower() == 'e' or \
-            palavras[i].lower() == 'i' or palavras[i].lower() == 'o' or \
-            palavras[i].lower() == 'u':
-                vogais = palavras[i]
-        print(vogais, end = " ")
+    for letras in palavras:
+        if letras.lower() in "aeiou":
+            if letras not in vogais:
+                vogais = letras
+            print(f"{vogais}", end="")
