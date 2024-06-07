cee1a860b88e2031e6c17de8bc923bb9d9f3a086
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-07 17:55:56 -0300

    Res: Desafio57 | Mundo II

--- Desafio57.py
+++ Desafio57.py
@@ -3,4 +3,4 @@ i = 0
 while sexo  != "m" and sexo != "f":
     sexo = input('Menu:\nm - Marculino\nf - Feminino\n>> ').strip().lower()
     i += 1
-print(f'Entrada aceita para: {sexo}')
+print(f'Entrada aceita para: {sexo} ')
