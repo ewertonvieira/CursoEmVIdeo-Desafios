73d9f006b70f557c7177d06eb6e729f73bf86c74
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-09 19:09:46 -0300

    Res: Desafio70 | Mundo II

--- Desafio70.py
+++ Desafio70.py
@@ -1,32 +1,33 @@
 total = 0.0
 count1 = 0
-nome_barato = ""; precobarato = 0.0
+count = 0
+nome_barato = ""  # Inicialize a variável nome_barato
+menorpreco = 0.0
 print("-"*60)
 print("LOJA SUPER BARATAO".center(57))
 print("-"*60)
 while True:
-    nomeprodu = input("Nome do produto: ")
+    nome_produto = input("Nome do produto: ")
     preco = float(input("Valor do produto: R$ "))
-    continuar = input("Deseja continuar? [S/N]: ")
-    continuar = continuar.upper()
+    continuar = input("Deseja continuar? [S/N]: ").upper()
     if continuar == "N":
         break
     else:
-    # Total da comprar
+        count += 1
+        # Total da compra
         total += preco
-    # Numero de produtos que custam mais de 1000 reais
+        # Numero de produtos que custam mais de 1000 reais
         if preco > 1000:
             count1 += 1
-    # Nome do produto mais barato
-        if precobarato is None or preco < precobarato:
-            precobarato = preco
-            nome_barato = nomeprodu
+        # Nome do produto mais barato
+        if count == 1 or preco < menorpreco:
+            nome_barato = nome_produto
+            menorpreco = preco
+    print("-"*60)
+    print("Novo cadastro...".center(57))
+    print("-"*60)
 
-        #
-        print("-"*60)
-        print("Novo cadastro...".center(57))
-        print("-"*60)
 print("-"*60)
-print(f"O total da compra foi R$ {total}")
-print(f"Temos {count1} produto custando mais de R$ 1000.00")
-print(f"O produto mais barato foi a {nome_barato} custando R$ {precobarato}")
+print(f"O total da compra foi R$ {total:.2f}")
+print(f"Temos {count1} produto(s) custando mais de R$ 1000.00")
+print(f"O produto mais barato foi '{nome_barato}' custando R$ {menorpreco:.2f}")
