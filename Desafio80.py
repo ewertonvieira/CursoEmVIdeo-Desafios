fc4f2a31d7063ada5f4cd0d5f86cec787c1a6312
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-17 14:38:28 -0300

    Res: Desafio80 | Mundo II

--- Desafio80.py
+++ Desafio80.py
@@ -1,16 +1,14 @@
-value = []
-for _ in range(5):  # Itera 5 vezes para adicionar 5 números
-    num = int(input('Informe um número: '))
-    if not value:  # Se a lista estiver vazia, apenas adiciona o número
-        value.append(num)
+listanum = []
+for _ in range(5):
+    num = int(input('Informe um numero: '))
+    if not listanum: # add num ao inicio da lista se a listanum estiver fazia
+        listanum.append(num)
     else:
-        inserted = False  # Variável para verificar se o número foi inserido
-        for i, item in enumerate(value):  # Itera sobre os elementos e índices na lista
-            if num <= item:  # Encontra o lugar certo para inserir o número mantendo a ordem crescente
-                value.insert(i, num)
-                inserted = True
-                break
-        if not inserted:  # Se não foi inserido, adiciona ao final da lista
-            value.append(num)
-
-print(value)
+        inserido = False # instancia inserido.
+        for i, item in enumerate(listanum):
+            if item >= num:
+                listanum.insert(i, num)
+                inserido = True # se o valor foi insetido o valor se torna True
+        if inserido == False:
+            listanum.append(num)
+print(listanum)
