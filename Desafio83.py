3e0a804cd3c6edfa83961aa562482cb820aca802
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-18 19:45:59 -0300

    83

---
+++ Desafio83.py
@@ -0,0 +1,8 @@
+count = 0
+carac = str(input('Informe uma expressao matematica: ')).replace(' ', '')
+for paren in carac:
+    if paren == '(':
+        count += 1
+    elif paren == ')':
+        count -= 1
+print(f'Expressão {carac} valida' if count == 0 else f'Expressão {carac} invalida')
