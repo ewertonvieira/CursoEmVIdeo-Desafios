18f8a0d192a7b1acb70465cefc9acee174579b24
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-18 20:56:09 -0300

    Res: Desafio83 | Mundo II

--- Desafio83.py
+++ Desafio83.py
@@ -1,8 +1,12 @@
 count = 0
 carac = str(input('Informe uma expressao matematica: ')).replace(' ', '')
-for paren in carac:
-    if paren == '(':
-        count += 1
-    elif paren == ')':
-        count -= 1
-print(f'Expressão {carac} valida!' if count == 0 else f'Expressão {carac} invalida!')
+carac = carac.split()
+print(carac)
+for item in carac:
+    print(item)
+    for paren in item:
+        if paren == '(':
+            count += 1
+        elif paren == ')':
+            count -= 1
+print(f'Expressão {item} valida!' if count == 0 else f'Expressão {item} invalida!')
