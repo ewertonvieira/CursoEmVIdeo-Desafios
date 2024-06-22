61511477ea94af8d93b07aa2282de2a40906ebab
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-22 13:20:40 -0300

    Res: Desafio84 | Mundo II

--- Desafio84.py
+++ Desafio84.py
@@ -2,23 +2,27 @@ pessoas = []; maispesadas = []; maisleves = []
 count = 0
 
 while True:
-    print('*'*20)
+    print('*'*27)
     nome = input('INFORME NOME: ')
     peso = float(input('INFORME NOME(Kg): '))
     pessoas.append([nome, peso])
     op = str(input('Deseja continuar?[S/N]... ')).upper()
     if op in "N":
         break
-pesos = [pessoa[1] for pessoa in pessoas]
-pesomax = max(pesos)
-pesomin = min(pesos)
-
+# Captura o mair valor peso na lista pessoas
+pesomax = max([peso[1] for peso in pessoas])
+pesomin = max([peso[1] for peso in pessoas])
+# Captura os iguais a pesso max e min na lista pessoas
 for pessoa in pessoas:
     if pessoa[1] == pesomax:
-        maispesadas.append(pessoa[:])
+        maispesadas.append(pessoa)
     if pessoa[1] == pesomin:
-        maisleves.append(pessoa[:])
-print(maispesadas)
-print(maisleves)
-print('*'*60)
-print(f'Ao todo, voce cadastrou {len(pessoas)}')
+        maisleves.append(pessoa)
+# Captura os nomes das pessoas mais pesadas e leves
+pessoamax = [pmax[0] for pmax in maispesadas]
+pessoamin = [pmin[0] for pmin in maisleves]
+# Printa estatisticas formatadas
+print('_-_'*25)
+print(f'Numero de cadastros {len(pessoas)}')
+print(f'Maior peso {pesomax} kg, pessoas mais pesadas {', '.join(pessoamax)}')
+print(f'Menor peso {pesomin} kg, pessoas mais leves {', '.join(pessoamin)}')
