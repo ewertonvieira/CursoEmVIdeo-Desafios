e1631b9f9194ffa19bc2aefbe9d40fb50503ca4f
ewertonvieira <ewertonvieira2401@gmail.com>
2024-06-23 21:00:27 -0300

    v

---
+++ Desafio85.py
@@ -0,0 +1,9 @@
+numlist = [[], []]
+for i in range(1, 8):
+    numb = int(input(f'Informe o {i}º valor: '))
+    if numb % 2 == 0:
+        numlist[0].append(numb)
+    else:
+        numlist[1].append(numb)
+print('_-_'*20)
+print(f'Os valores pares digitados foram: {sorted(numlist[0])}\nOs valores impares digitados foram: {sorted(numlist[1])}')
